fcce174fa8d7e93d2de6bedf0b6b868a24a11e14
Ben Parizek <ben.parizek@gmail.com>
2024-05-30 08:51:35 -0400

    Prep release

--- src/SproutSitemaps.php
+++ src/SproutSitemaps.php
@@ -18,9 +18,9 @@ use yii\base\Event;
 
 class SproutSitemaps extends Plugin implements SproutPluginMigrationInterface
 {
-    public string $minVersionRequired = '1.3.0';
+    public string $minVersionRequired = '4.45.3';
 
-    public string $schemaVersion = '4.45.0';
+    public string $schemaVersion = '5.0.0';
 
     public static function editions(): array
     {
--- src/migrations/m240530_000000_schema_5_0_0.php
+++ src/migrations/m240530_000000_schema_5_0_0.php
@@ -4,9 +4,10 @@ namespace BarrelStrength\SproutSitemaps\migrations;
 
 use BarrelStrength\Sprout\core\db\m000000_000000_sprout_plugin_migration;
 use BarrelStrength\Sprout\core\db\SproutPluginMigrationInterface;
+use BarrelStrength\SproutDataStudio\SproutDataStudio;
 use BarrelStrength\SproutSitemaps\SproutSitemaps;
 
-class m240309_034136_schema_4_45_0 extends m000000_000000_sprout_plugin_migration
+class m240530_000000_schema_5_0_0 extends m000000_000000_sprout_plugin_migration
 {
     public function getPluginInstance(): SproutPluginMigrationInterface
     {
