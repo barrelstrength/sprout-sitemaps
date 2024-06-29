2a10880e7ec2d385d274b4d833eda195c3ef2703
Ben Parizek <ben.parizek@gmail.com>
2024-06-29 08:40:38 -0400

    Code cleanup

--- src/migrations/m240530_000000_schema_5_0_0.php
+++ src/migrations/m240530_000000_schema_5_0_0.php
@@ -4,7 +4,6 @@ namespace BarrelStrength\SproutSitemaps\migrations;
 
 use BarrelStrength\Sprout\core\db\m000000_000000_sprout_plugin_migration;
 use BarrelStrength\Sprout\core\db\SproutPluginMigrationInterface;
-use BarrelStrength\SproutDataStudio\SproutDataStudio;
 use BarrelStrength\SproutSitemaps\SproutSitemaps;
 
 class m240530_000000_schema_5_0_0 extends m000000_000000_sprout_plugin_migration
